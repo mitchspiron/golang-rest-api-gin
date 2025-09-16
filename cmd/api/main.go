0a65fe55a52315be032851a98a37c7e1f1f8ff00
mitchspiron <mitchspiron280@gmail.com>
2025-09-16 20:48:49 +0300

    feat: set up GIN server and env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"database/sql"
+	"golang-rest-api-gin/internal/database"
+	"golang-rest-api-gin/internal/env"
 	"log"
+
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type application struct {
+	port      int
+	jwtSecret string
+	models    database.Models
+}
 
 func main() {
 	db, err := sql.Open("sqlite3", "./data.db")
-
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer db.Close()
+
+	models := database.NewModels(db)
+
+	app := &application{
+		port:      env.GetEnvInt("PORT", 4000),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "supersecretkey"),
+		models:    models,
+	}
+
+	if err := serve(app); err != nil {
+		log.Fatal(err)
+	}
 }
