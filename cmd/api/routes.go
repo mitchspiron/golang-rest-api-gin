088ee960a988b6bbfbf4b7a66151d5592399ce0f
mitchspiron <mitchspiron280@gmail.com>
2025-09-18 15:48:03 +0300

    feat: protect routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,23 +12,31 @@ func (app *application) routes() http.Handler {
 	v1 := g.Group("/api/v1")
 	{
 		// Event routes
-		v1.POST("/events", app.createEvent)
 		v1.GET("/events", app.getAllEvents)
 		v1.GET("/events/:id", app.getEvent)
-		v1.PUT("/events/:id", app.updateEvent)
-		v1.DELETE("/events/:id", app.deleteEvent)
-		v1.POST("/events/:id/attendees/:userId", app.addAttendeeToEvent)
-		v1.GET("/events/:id/attendees", app.getAttendeesForEvent)
 
 		// User registration route
 		v1.POST("/auth/register", app.registerUser)
 
 		// Attendee routes
-		v1.DELETE("/events/:id/attendees/:userId", app.deleteAttendeeFromEvent)
 		v1.GET("/attendees/:id/events", app.getEventsByAttendee)
 
 		// Login route
 		v1.POST("/auth/login", app.login)
+	}
+
+	// Protected routes
+	authGroup := g.Group("/")
+	authGroup.Use(app.authMiddleware())
+	{
+		// Event routes
+		authGroup.POST("/events", app.createEvent)
+		authGroup.PUT("/events/:id", app.updateEvent)
+		authGroup.DELETE("/events/:id", app.deleteEvent)
+		authGroup.POST("/events/:id/attendees/:userId", app.addAttendeeToEvent)
+
+		// Attendee routes
+		authGroup.DELETE("/events/:id/attendees/:userId", app.deleteAttendeeFromEvent)
 	}
 
 	return g
